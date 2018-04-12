a04720eeb384000ec9a4dd917667c06c4cea7e0b
Edoardo Debenedetti <dedeswim@gmail.com>
2018-04-13 00:37:56 +0800

    Add createParty page

--- lib/create_party.dart
+++ lib/create_party.dart
@@ -11,45 +11,115 @@ class CreatePartyPage extends StatefulWidget {
 }
 
 class _CreatePartyPageState extends State<CreatePartyPage> {
+  bool _chosenImage = false;
   @override
   Party party = new Party();
   Widget build(BuildContext context) {
     return new Scaffold(
-        appBar: new AppBar(
-          title: new Text('New party'),
-        ),
-        body: new DropdownButtonHideUnderline(
-          child: new SafeArea(
-            top: false,
-            bottom: false,
-            child: Container(
-              decoration: new BoxDecoration(
-                color: primaryLight,
-              ),
-              child: new ListView(
-                padding: const EdgeInsets.all(16.0),
-                children: <Widget>[
-                  new TextField(
-                    enabled: true,
-                    decoration: const InputDecoration(
-                      labelText: 'Party name',
-                      labelStyle: TextStyle(
-                        fontSize: 38.0,
-                        fontWeight: FontWeight.bold,
-                        color: const Color(0x20FFFFFF),
-                      ),
-                    ),
-                    style: Theme.of(context).textTheme.headline.copyWith(
+      appBar: new AppBar(
+        title: new Text('New party'),
+      ),
+      body: new DropdownButtonHideUnderline(
+        child: new SafeArea(
+          top: false,
+          bottom: false,
+          child: Container(
+            decoration: new BoxDecoration(
+              color: primaryLight,
+            ),
+            child: new ListView(
+              children: <Widget>[
+                new Container(
+                  height: 150.0,
+                  child: new _PartyImageContainer(
+                    chosenImage: _chosenImage,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+                  child: Form(
+                    child: new TextFormField(
+                      enabled: true,
+                      decoration: const InputDecoration(
+                        labelText: 'Party name',
+                        labelStyle: TextStyle(
                           fontSize: 38.0,
                           fontWeight: FontWeight.bold,
-                          color: Colors.white,
+                          color: const Color(0x99FFFFFF),
                         ),
-                    maxLength: 20,
+                      ),
+                      style: Theme.of(context).textTheme.headline.copyWith(
+                            fontSize: 38.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                      maxLength: 20,
+                    ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
-        ));
+        ),
+      ),
+    );
+  }
+}
+
+class _PartyImageContainer extends StatefulWidget {
+  _PartyImageContainer({this.chosenImage});
+  final bool chosenImage;
+
+  @override
+  _PartyImageContainerState createState() =>
+      new _PartyImageContainerState(chosenImage: chosenImage);
+}
+
+class _PartyImageContainerState extends State<_PartyImageContainer> {
+  _PartyImageContainerState({this.chosenImage});
+
+  final bool chosenImage;
+
+  @override
+  Widget build(BuildContext context) {
+    if (chosenImage) {
+      return new Container(
+        decoration: new BoxDecoration(
+          image: new DecorationImage(
+            image: new AssetImage(
+              'assets/img/movingParty.jpeg',
+            ),
+            fit: BoxFit.cover,
+          ),
+        ),
+      );
+    }
+    return new Container(
+      decoration: new BoxDecoration(
+        color: const Color(0x99FFFFFF),
+      ),
+      child: new Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          new FlatButton(
+            child: new Icon(
+              Icons.photo,
+              color: secondary,
+              size: 45.0,
+            ),
+            onPressed: () => print("Pressed"),
+            shape: CircleBorder(),
+          ),
+          new Text(
+            'Add a picture!',
+            style: new TextStyle(
+              color: secondary,
+              fontSize: 26.0,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/theme.dart
+++ lib/theme.dart
@@ -10,13 +10,13 @@ Color secondary = const Color(0xFFE52059);
 Color secondaryLight = const Color(0xFFED2467);
 Color secondaryDark = const Color(0xFFB70449);
 
-// Functional colors
-Color divider= const Color(0x2F000000);
+// Functional colors 
+Color divider = const Color(0x2F000000);
+Color inputFieldColor = const Color(0x99FFFFFF);
 
 ThemeData pinderyTheme = new ThemeData(
   primaryColor: primary,
   accentColor: secondary,
   buttonColor: secondaryLight,
-  dividerColor: divider,
   buttonTheme: new ButtonThemeData(textTheme: ButtonTextTheme.accent),
 );
