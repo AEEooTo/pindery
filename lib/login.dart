45c7cd089efa5880202410bd1927ad326bd26aca
Anna Tranquillini <s235372@studenti.polito.it>
2018-04-16 10:02:32 +0800

    Login page update

--- lib/login.dart
+++ lib/login.dart
@@ -68,6 +68,7 @@ class _LoginPageState extends State<LoginPage> {
                                       labelStyle: TextStyle(
                                         color: dividerColor,
                                         fontSize: 30.0,
+                                        fontWeight: FontWeight.w300
                                       ),
                                       border: const UnderlineInputBorder(
                                           borderSide: const BorderSide(
@@ -83,6 +84,7 @@ class _LoginPageState extends State<LoginPage> {
                                   maxLength: 20,
                                 ),
                               ),
+<<<<<<< Updated upstream
                               new TextFormField(
                                 // TODO: hide password characters!!1!!1
                                 controller: passwordController,
@@ -107,10 +109,21 @@ class _LoginPageState extends State<LoginPage> {
                                   color: dividerColor,
                                 ),
                                 maxLength: 20,
+=======
+                              new PasswordField(
+                                labelText: 'Password',
+                                helperText: 'Insert your password',
+                                onFieldSubmitted: (String value) {
+                                  setState(() {
+                                    password = value;
+                                  });
+                                },
+
+>>>>>>> Stashed changes
                               ),
                               new Padding(
                                 padding: const EdgeInsets.only(top: 80.0),
-                                child: new WelcomeButton(
+                                child: new LogInButton(
                                   text: '  LOG IN  ',
                                   color: primary,
                                 ),
@@ -129,13 +142,10 @@ class _LoginPageState extends State<LoginPage> {
       ),
     );
   }
-
-
-
 }
 
-class WelcomeButton extends StatelessWidget{
-  WelcomeButton({this.text, this.color});
+class LogInButton extends StatelessWidget{
+  LogInButton({this.text, this.color});
 
   final String text;
   final Color color;
@@ -151,9 +161,126 @@ class WelcomeButton extends StatelessWidget{
             color: Colors.white
         ),),
       onPressed: (){
-
-
+        Navigator.push(
+          context,
+          new MaterialPageRoute(
+              builder: (context) => new LogingInPage()),
+        );
       },
+    );
+  }
+
+}
+
+class PasswordField extends StatefulWidget {
+  const PasswordField({
+
+    this.hintText,
+    this.labelText,
+    this.helperText,
+    this.onSaved,
+    this.validator,
+    this.onFieldSubmitted,
+  });
+
+
+  final String hintText;
+  final String labelText;
+  final String helperText;
+  final FormFieldSetter<String> onSaved;
+  final FormFieldValidator<String> validator;
+  final ValueChanged<String> onFieldSubmitted;
+
+  @override
+  _PasswordFieldState createState() => new _PasswordFieldState();
+}
+
+class _PasswordFieldState extends State<PasswordField> {
+  bool _obscureText = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return new TextFormField(
+
+      obscureText: _obscureText,
+      maxLength: 10,
+      onSaved: widget.onSaved,
+      validator: widget.validator,
+      onFieldSubmitted: widget.onFieldSubmitted,
+      decoration: new InputDecoration(
+        border: const UnderlineInputBorder(),
+        filled: true,
+        hintText: widget.hintText,
+        hintStyle: new TextStyle(
+        color: dividerColor,
+        fontSize: 30.0,
+        ),
+        labelText: widget.labelText,
+        labelStyle: new TextStyle(
+          color: dividerColor,
+          fontSize: 30.0,
+            fontWeight: FontWeight.w300
+        ),
+        helperText: widget.helperText,
+        helperStyle: new TextStyle(
+          color: dividerColor,
+          fontSize: 14.0,
+        ),
+        suffixIcon: new GestureDetector(
+          onTap: () {
+            setState(() {
+              _obscureText = !_obscureText;
+            });
+          },
+          child: new Icon(_obscureText ? Icons.visibility : Icons.visibility_off),
+        ),
+      ),
+    );
+  }
+}
+
+class LogingInPage extends StatelessWidget{
+  LogingInPage({Contex});
+  Widget build(BuildContext context){
+    return new Scaffold(
+      drawer: new Drawer(
+        child: new PinderyDrawer(),
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        decoration: new BoxDecoration(
+            color: Colors.white
+        ),
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only( top: 96.0),
+              child: new Container(
+                height: 214.0,
+                width: 214.0,
+                decoration: new BoxDecoration(
+
+                    image: new DecorationImage(
+                      image: new AssetImage('assets/img/logo_v_2_rosso.png'),
+                      fit: BoxFit.fitHeight,
+                    )
+                ),),
+            ),
+            new Padding(
+              padding: const EdgeInsets.only(bottom: 81.0),
+              child: new Text(
+                'Logging in!',
+                style: new TextStyle(
+                    fontSize: 40.0,
+                    color: secondary,
+                    fontWeight: FontWeight.w600
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 
