762f632910e57c974ffe13c4f7c28bf3deab8e3c
Edoardo Debenedetti <dedeswim@gmail.com>
2018-04-25 23:06:34 +0800

    Update some dependencies, add caching for user images, now pics are full

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,5 +1,5 @@
 /// Some utils for the app
-/// 
+///
 
 // External libraries imports
 import 'package:flutter/material.dart';
@@ -7,26 +7,59 @@ import 'package:flutter/material.dart';
 // Internal imports
 import 'user.dart';
 import 'theme.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 /// A [CircleAvatar] which checks whether the user has a profile picture or not
-class PinderyCircleAvatar extends StatelessWidget{
-  PinderyCircleAvatar({this.user});
+/// Actually if the user has a picture, it's not a [CircleAvatar], but a 
+/// [MaterialType.circle], so that the image can be cached with a [CachedNetworkImage].
+/// There is also the user's initial as placeholder while downloading and in case of error.
+class PinderyCircleAvatar extends StatelessWidget {
+  PinderyCircleAvatar({this.user, this.radius = 72.0});
+  /// The [User] whose image/name is used for the widget
   final User user;
+
+  // The the radius of the circle
+  final double radius;
 
   @override
   Widget build(BuildContext context) {
     if (user.profilePictureUrl == null) {
       return new CircleAvatar(
-        child: new Text(
-          user.name[0],
-          style: new TextStyle(color: Colors.white, fontSize: 30.0),
-          ),
+        radius: radius,
+        child: new UserInitialWidget(name: user.name),
         backgroundColor: secondary,
       );
     }
-    return new CircleAvatar(
-      backgroundImage: new NetworkImage(user.profilePictureUrl),
-      backgroundColor: secondary,
+    return new Container(
+      height: radius,
+      width: radius,
+      child: new Material(
+        color: secondary,
+        type: MaterialType.circle,
+        child: new CachedNetworkImage(
+          fit: BoxFit.fill,
+          imageUrl: user.profilePictureUrl,
+          placeholder: new UserInitialWidget(name: user.name),
+          errorWidget: new UserInitialWidget(name: user.name),
+        ),
+      ),
+    );
+  }
+}
+
+/// A [Widget] that returns a [Center] widget with the initial of the name passed
+/// as parameter, to be used with [PinderyCircleAvatar].
+class UserInitialWidget extends StatelessWidget {
+  UserInitialWidget({this.name});
+  final String name;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Center(
+      child: new Text(
+        name[0],
+        style: new TextStyle(color: Colors.white, fontSize: 30.0),
+      ),
     );
   }
 }
